da0d93eeb1aba6bbfcca2dbf89c5cef557c46eca
ecandotti <enzo.candotti16@hotmail.com>
2021-01-29 10:08:57 +0100

    leafletjs implementation

---
+++ src/components/MapPeople.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const MapPeople = ({ lat, lon, fullname }) => {
+    return (
+        <div>
+            <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[lat, lon]}>
+                    <Popup>{fullname} lives here !</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
+
+MapPeople.propTypes = {
+    lat: PropTypes.number.isRequired,
+    lon: PropTypes.number.isRequired,
+    fullname: PropTypes.string.isRequired,
+}
+
+export default MapPeople
