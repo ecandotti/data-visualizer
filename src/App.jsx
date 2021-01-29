7159e100678e1b4b1ec366d936a9a907b95e36bf
ecandotti <enzo.candotti16@hotmail.com>
2021-01-29 10:09:35 +0100

    useContext for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import { DataContext } from './context/data'
+
+import data from './data/people.json'
+
 import Navigation from './components/Navigation'
-import PeopleCard from './components/PeopleCard'
-import Temp from './views/Temp'
 
 import Home from './views/Home'
 import Analyze from './views/Analyze'
+import PeopleCard from './views/PeopleCard'
 
 const App = () => {
+    const [people, setPeople] = useState(data.people)
     return (
         <div className="container">
-            <h1 style={{ textAlign: 'center' }}>Data Visualizer 🔬</h1>
-            <Router>
-                <Navigation />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/analyze" component={Analyze} />
-                    <Route path="/temp" component={Temp} />
-                    <Route path="/people/:id" component={PeopleCard} />
-                    <Route path="*" component={() => '404'} />
-                </Switch>
-            </Router>
+            <h1 id="main-title">Data Visualizer 🔬</h1>
+            <DataContext.Provider value={{ people, setPeople }}>
+                <Router>
+                    <Navigation />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/analyze" component={Analyze} />
+                        <Route path="/people/:id" component={PeopleCard} />
+                        <Route path="*" component={() => '404'} />
+                    </Switch>
+                </Router>
+            </DataContext.Provider>
         </div>
     )
 }
