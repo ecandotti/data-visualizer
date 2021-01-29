73552b9c28b458c8a187b21f0a08b829003035a6
ecandotti <enzo.candotti16@hotmail.com>
2021-01-29 10:10:38 +0100

    useData + JSON download

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { AiFillHome, AiFillPieChart } from 'react-icons/ai'
 import { FaFileExport } from 'react-icons/fa'
-import { NavLink } from 'react-router-dom'
-
-// import data from '../data/people.json'
+import { TiArrowBack } from 'react-icons/ti'
+import { NavLink, useHistory } from 'react-router-dom'
+import { useData } from '../context/data'
 
 const Navigation = () => {
+    const { people } = useData()
+    const history = useHistory()
+    const throwBack = () => {
+        history.goBack()
+    }
+
+    const exportFiles = () => {
+        const filename = 'data.json'
+        const jsonStr = JSON.stringify(people)
+        let element = document.getElementById('download')
+        element.setAttribute(
+            'href',
+            'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr)
+        )
+        element.setAttribute('download', filename)
+    }
+
     return (
         <nav>
             <NavLink exact to="/">
@@ -14,12 +31,15 @@ const Navigation = () => {
             <NavLink to="/analyze">
                 <AiFillPieChart />
             </NavLink>
-            <span>
+            <a id="download" onClick={() => exportFiles()}>
                 <FaFileExport />
-            </span>
-            <NavLink to="/temp">
-                <AiFillHome />
-            </NavLink>
+            </a>
+            <a
+                onClick={() => throwBack()}
+                style={{ backgroundColor: '#3498db' }}
+            >
+                <TiArrowBack />
+            </a>
         </nav>
     )
 }
