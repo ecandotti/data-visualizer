8d622f3b289d4f1820abad8f382a5b91ce5fb73b
ecandotti <enzo.candotti16@hotmail.com>
2021-03-16 16:19:45 +0100

    add fruit filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,35 +1,83 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useData } from '../context/dataContext'
-import Table from './Table'
 
 const Filter = () => {
-    const { people } = useData()
+    const { initialData, setPeople } = useData()
 
-    const [value, setValue] = useState('')
+    const [genderValue, setGenderValue] = useState('')
 
     let gender = []
-    for (let el of people) {
+    for (let el of initialData) {
         if (gender.indexOf(el.gender) === -1) {
             gender.push(el.gender)
         }
     }
 
-    const result = people.filter((word) => word.gender === value)
+    const [fruitValue, setFruitValue] = useState('')
+
+    let fruit = []
+    for (let el of initialData) {
+        if (fruit.indexOf(el.preferences.favorite_fruit) === -1) {
+            fruit.push(el.preferences.favorite_fruit)
+        }
+    }
+
+    useEffect(() => {
+        if (fruitValue === '') {
+            setPeople(initialData)
+        } else {
+            setGenderValue('')
+            setPeople(
+                initialData.filter(
+                    (word) => word.preferences.favorite_fruit === fruitValue
+                )
+            )
+        }
+    }, [fruitValue])
+
+    useEffect(() => {
+        if (genderValue === '') {
+            setPeople(initialData)
+        } else {
+            setFruitValue('')
+            setPeople(initialData.filter((word) => word.gender === genderValue))
+        }
+    }, [genderValue])
 
     return (
-        <div style={{ marginBottom: '20px', marginTop: '20px' }}>
-            <h2>Préférence</h2>
-            <span>Choose gender : </span>
-            <select value={value} onChange={(e) => setValue(e.target.value)}>
-                <option value=""></option>
-                {gender.map((item, key) => (
-                    <option key={key} value={item}>
-                        {item}
-                    </option>
-                ))}
-            </select>
-            {value && <Table data={result} />}
+        <div className="filter-container">
+            <h2 className="filter-title">Préférence</h2>
+            <div className="filter-menu">
+                <div className="filter-item">
+                    <span>Choose gender : </span>
+                    <select
+                        value={genderValue}
+                        onChange={(e) => setGenderValue(e.target.value)}
+                    >
+                        <option value=""></option>
+                        {gender.map((item, key) => (
+                            <option key={key} value={item}>
+                                {item}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="filter-item">
+                    <span>Choose fruit : </span>
+                    <select
+                        value={fruitValue}
+                        onChange={(e) => setFruitValue(e.target.value)}
+                    >
+                        <option value=""></option>
+                        {fruit.map((item, key) => (
+                            <option key={key} value={item}>
+                                {item}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
         </div>
     )
 }
